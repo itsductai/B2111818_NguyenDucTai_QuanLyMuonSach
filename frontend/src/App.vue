e37023ae9b3a6a0b0f0f3ff37de822e44b68af65
Nguyen Duc Tai <ductaaii.2002@gmail.com>
2025-03-26 04:23:27 +0700

    Cập nhật App.vue để thêm navigation

---
+++ frontend/src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  name: 'App',
+  components: {
+    RouterLink,
+    RouterView
+  }
+}
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">Quản lý mượn sách</RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/">Trang chủ</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/sach">Quản lý sách</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <RouterView />
+</template>
+
+<style>
+.navbar {
+  margin-bottom: 20px;
+}
+</style>
